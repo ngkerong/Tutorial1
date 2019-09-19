89b5806273cdfb012c053386f19e2a2d1fba6ea6
ngkerong <55265526+ngkerong@users.noreply.github.com>
2019-09-19 23:47:41 +0800

    Version 1

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+
+    public static void main(String[]args){
+
+        int A = 3*6*1/2;
+        int B = 3*4*1/2;
+        int C = 4*3;
+        int Total = A+B+C;
+        
+        System.out.println("A= " +A);
+        System.out.println("B= " +B);
+        System.out.println("C= " +C);
+        System.out.println("Total= " +Total);
+
+    
+    }
+
+}
